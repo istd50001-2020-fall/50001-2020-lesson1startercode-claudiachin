65a9591a8dc7cd30d9cd8d5437e7faca4f90a1f5
claudiachin <claudia.chinsh@gmail.com>
2020-11-05 14:56:47 +0800

    done

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,14 @@
-package com.example.myfirstapp;
-
-// Create a new android studio project with Empty Activity
-// Copy the code below
-// Go to your own MainActivity.java and
-// paste it over the existing code BELOW the package statement ***
-// ***Sep 2020
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 //TODO 1.1 Put in some images in the drawables folder
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> images;
     Button charaButton;
     ImageView charaImage;
+    Button fCharaButton;
     int count = 0;
 
     @Override
@@ -28,13 +29,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO 1.3 Instantiate An ArrayList object
+        images = new ArrayList();
+
         //TODO 1.4 Add the image IDs to the ArrayList
+        images.add(R.drawable.ashketchum);
+        images.add(R.drawable.bartsimpson);
+        images.add(R.drawable.edogawaconan);
+        images.add(R.drawable.judyhopps);
+        images.add(R.drawable.nemo);
+        images.add(R.drawable.pikachu);
+        images.add(R.drawable.snorlax);
+        images.add(R.drawable.tomandjerry);
+        images.add(R.drawable.yoda);
+
         //TODO 1.5 Get references to the charaButton and charaImage widgets using findViewById
+        charaImage = findViewById(R.id.RandomImage);
+        charaButton = findViewById(R.id.RandomImageButton);
+
         //TODO 1.6 For charaButton, invoke the setOnClickListener method
         //TODO 1.7 Create an anonymous class which implements View.OnClickListener interface
         //TODO 1.8 Within onClick, write code to randomly select an image ID from the ArrayList and display it in the ImageView
-        //TODO 1.9 [On your own] Create another button, which when clicked, will cause one image to always be displayed
+        charaButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                int id = images.get(r.nextInt(images.size()));
+                charaImage.setImageResource(id);
+            }
+        });
 
+        //TODO 1.9 [On your own] Create another button, which when clicked, will cause one image to always be displayed
+        fCharaButton = findViewById(R.id.FirstImageButton);
+        fCharaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = images.get(0);
+                charaImage.setImageResource(id);
+            }
+        });
 
     }
 }
